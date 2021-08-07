bfc94e254618900616158749a5deba849d7f936a
SonGohan-isme <dongphuong.meou19@gmail.com>
2021-08-07 18:42:16 +0700

    update form claim

--- src/app/view/claim-request-manage/form-notice-claim/form-notice-claim.component.ts
+++ src/app/view/claim-request-manage/form-notice-claim/form-notice-claim.component.ts
@@ -8,6 +8,13 @@ import { ContractrequestService } from 'src/app/services/contractRequest/contrac
 import { ContractService } from 'src/app/services/contract/contract.service';
 import { Request } from 'src/app/model/Request';
 import { Contract } from 'src/app/model/Contract';
+import { RequestClaimApprove } from 'src/app/model/RequestClaimApprove';
+import { CustomerService } from 'src/app/services/customer/customer.service';
+import { CustomerInfo } from 'src/app/model/CustomerInfo';
+import { IllustrationService } from 'src/app/services/illustration/illustration.service';
+import { Illustration } from 'src/app/model/Illustration';
+import { IllustrationSubBenifit } from 'src/app/model/IllustrationSubBenifit';
+import { RelatedPersonInfo } from 'src/app/model/RelatedPersonInfo';
 
 @Component({
   selector: 'app-form-notice-claim',
@@ -17,11 +24,12 @@ import { Contract } from 'src/app/model/Contract';
 export class FormNoticeClaimComponent implements OnInit {
 
   employeinfoDTO: EmployeeInfoDTO;
-  req: Request;
+  req: RequestClaimApprove;
   contract: Contract;
+  custInfo: Array<CustomerInfo>;
 
   constructor(private common: CommonService, private employeeService: EmployeeService, private activateRoute: ActivatedRoute,
-    private contractRequestService: ContractrequestService, private contractService: ContractService) { }
+    private contractRequestService: ContractrequestService, private contractService: ContractService, private custService: CustomerService) { }
 
   ngOnInit(): void {
 
@@ -34,9 +42,15 @@ export class FormNoticeClaimComponent implements OnInit {
       let data1 = this.req.id_contract;
       this.contractService.getDetailContractForCustomer(data1).subscribe((data1 => {
         this.contract = data1;
+        this.custService.getDetailCustomerInfoAdmin(this.contract.id_customer).subscribe((data2 => {
+          this.custInfo = data2;
+        }))
       }))
     }))
+  }
 
+  dowloadPDF() {
+    window.print();
   }
 
 }
